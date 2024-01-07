3e4a837477566e9f25e8c42772413e537bda6ad7
Bradley Shiver <ltlongrods@live.com>
2024-01-07 12:16:34 -0500

    these files didn't get added

---
+++ MessageManager.cpp
@@ -0,0 +1,85 @@
+#include "MessageManager.h"
+#include "MessageListener.h"
+#include <windows.h>
+
+MessageManager* MessageManager::s_Instance = nullptr;
+
+MessageManager::MessageManager() 
+{
+	if (MessageManager::s_Instance == nullptr)
+		MessageManager::s_Instance = this;
+}
+
+MessageManager::~MessageManager() {}
+
+MessageManager* MessageManager::GetInstance()
+{
+	if (MessageManager::s_Instance == nullptr)
+		MessageManager::s_Instance = new MessageManager;
+
+	return MessageManager::s_Instance;
+}
+
+void MessageManager::DeleteInstance()
+{
+	MessageManager::s_Instance = nullptr;
+}
+
+bool MessageManager::Register(MessageListener* listener, MessageType msgType)
+{
+	if (!m_subscriptions.count(msgType))
+	{   
+		std::set<MessageListener*> listeners;
+		m_subscriptions.insert(std::make_pair(msgType, listeners));
+	}
+
+	/*std::vector<MessageListener*> listeners = m_subscriptions.find(msgType)->second;
+
+	for (std::vector<MessageListener*>::iterator it = listeners.begin(); it != listeners.end(); ++it)
+	{
+		if ((*it) == listener)
+		{
+			OutputDebugString(L"Failed to register listener - already registered.");
+			return false;
+		}
+	}
+
+	listeners.push_back(listener);*/
+
+	m_subscriptions.find(msgType)->second.insert(listener);
+
+	return true;
+}
+
+bool MessageManager::Unregister(MessageListener* listener, MessageType msgType)
+{
+	//todo
+
+	return true;
+}
+
+bool MessageManager::QueueMessage(Message msg)
+{
+	m_messageQueue.push(msg);
+
+	return true;
+}
+
+bool MessageManager::SendMessages()
+{
+	while (!m_messageQueue.empty())
+	{
+		Message currentMessage = m_messageQueue.front();
+
+		std::set<MessageListener*> listeners = m_subscriptions.find(currentMessage.GetMessageType())->second;
+
+		for (auto it = listeners.begin(); it != listeners.end(); ++it)
+		{
+			(*it)->HandleMessage(currentMessage);
+		}
+
+		m_messageQueue.pop();
+	}
+
+	return true;
+}
---
+++ MessageManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Message.h"
+#include <set>
+#include <map>
+#include <queue>
+
+class MessageListener;
+
+class MessageManager
+{
+public:
+	MessageManager();
+	~MessageManager();
+
+	static MessageManager* GetInstance();
+	static void DeleteInstance();
+
+	bool Register(MessageListener*, MessageType);
+	bool Unregister(MessageListener*, MessageType);
+	bool QueueMessage(Message);
+	bool SendMessages();
+
+private:
+	static MessageManager* s_Instance;
+
+	std::queue<Message> m_messageQueue;
+	std::map<MessageType, std::set<MessageListener*>> m_subscriptions;
+};
